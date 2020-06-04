cef5392e1ddb40d1fa88a487a9ded9ef7121347d
Damir Gafic <damirgafic@gmail.com>
2020-06-04 10:29:29 -0700

    update

--- pdf.py
+++ pdf.py
@@ -1,4 +1,22 @@
 import PyPDF2
+import sys
 
-with open('dummy.pdf', 'r') as file:
-    print(file)
+inputs = sys.argv[1:]
+
+def pdf_combine(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('combined.pdf')
+
+pdf_combine(inputs)
+
+#with open('dummy.pdf', 'rb') as file:
+#    reader = PyPDF2.PdfFileReader(file)
+#    page = reader.getPage(0)
+#    page.rotateClockwise(180)
+#    writer = PyPDF2.PdfFileWriter()
+#    writer.addPage(page)
+#    with open('tilt.pdf', 'wb') as new_file:
+#        writer.write(new_file)
